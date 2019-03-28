892b495192de925bae8ecf3c6e223405567ad80b
JasonPer <m15191678056@163.com>
2019-03-28 09:33:02 +0800

    add controller 2

--- src/main/java/org/niit/soft/gitpracticedemo/controller/StudentController.java
+++ src/main/java/org/niit/soft/gitpracticedemo/controller/StudentController.java
@@ -1,10 +1,12 @@
 package org.niit.soft.gitpracticedemo.controller;
 
 import org.niit.soft.gitpracticedemo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+    @GetMapping("/hello")
     public Student helloStudent(){
         Student student = new Student();
         student.setUsername("李huofang");

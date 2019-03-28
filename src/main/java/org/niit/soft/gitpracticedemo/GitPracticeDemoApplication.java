a52455304fa299d8e660a12467e6fd1b112e500b
huofangLi <2895844821@qq.com>
2019-03-28 09:05:36 +0800

    git-practice-demo first commit

---
+++ src/main/java/org/niit/soft/gitpracticedemo/GitPracticeDemoApplication.java
@@ -0,0 +1,13 @@
+package org.niit.soft.gitpracticedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPracticeDemoApplication.class, args);
+    }
+
+}
